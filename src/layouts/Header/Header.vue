36cf2fbcccefd18a00c9a47eb6e57029ee808692
Enes Burak Dikici <dikicienesburak1@gmail.com>
2025-12-01 17:08:55 +0300

    Configuration documents have been changed.

--- src/layouts/Header/Header.vue
+++ src/layouts/Header/Header.vue
@@ -1,12 +1,5 @@
-<script setup>
-
-</script>
-
 <template>
     <header class="sticky top-0 text-3xl font-semibold bg-black/10 text-center backdrop-blur-sm">
         <h1>Weather App</h1>
     </header>
 </template>
-
-<style scoped>
-</style>
